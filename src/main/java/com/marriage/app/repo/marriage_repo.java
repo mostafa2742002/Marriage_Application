340153e2098403a69b0b3b82dfe3b4397441b04b
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-03-07 10:34:47 +0200

    Add findById method to marriage_repo

--- src/main/java/com/marriage/app/repo/marriage_repo.java
+++ src/main/java/com/marriage/app/repo/marriage_repo.java
@@ -2,6 +2,7 @@ package com.marriage.app.repo;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import com.marriage.app.entities.User;
 @Repository
 public interface marriage_repo  extends MongoRepository <User, String>{
     public User findByEmail(String email);
-
+    public Optional<User> findById(String id);
     // i want to get the user by gender 
     public ArrayList<User> findByGender(String gender);
 }
