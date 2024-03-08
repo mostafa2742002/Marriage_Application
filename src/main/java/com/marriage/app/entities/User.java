662c73601fdff331400125fcbed3cc4569e89774
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-03-08 16:55:44 +0200

    Refactor User class: Add new fields for religious denomination, tribal affiliation, health status, educational level, marital status, work status, expected marriage date, favorite communication, wearing hijab, need for kids, smoking/drinking habits, skin color, religious commitment, daily habits, weight, and height.

--- src/main/java/com/marriage/app/entities/User.java
+++ src/main/java/com/marriage/app/entities/User.java
@@ -7,10 +7,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
+
 @Data
 @Document(collection = "users")
 public class User {
-    
+
     @Id
     private String id;
 
@@ -26,27 +27,51 @@ public class User {
 
     private String gender; // man or woman
 
-    private String study_level; // study level of the user (primary, secondary, university, etc.)
-
-    private String work_situation; // work situation of the user (student, employee, etc.)
-
     private String nationality; // nationality of the user
 
     private String live_situation; // live situation of the user (alone, with kids, with parents, etc.)
 
-    private String expected_marriage_date; // expected marriage date of the user (one year, two years, etc.)
-
     private String living_place; // living place of the user (my home, my parents home, etc.)
-
-    private String smoking; // if the user smokes or not
-
-    private String drinking;   // if the user drinks or not
 
     private String language_for_communication; // language for communication of the user (english, french, etc.)
 
     private String income; // income of the user
 
-    private String need_kids; // if the user needs kids or not
+    private String religious_denomination;// religious denomination of the user (سني او شيعي او اسماعيلي او جعفري)
 
+    private String tribal_affiliation; // tribal affiliation of the user (انتمي لعائلة قبلية او لا انتمي لعائلة قبلية
+                                       // او افضل الا اجيب)
+
+    private String health_status_woman_man; // health status of the user (في صحه جيده او اعاني من مرض مزمن )
+
+    private String educational_level_woman_man; // educational level of the user (primary, secondary, university, etc.)
+
+    private String marital_status_woman_man; // marital status of the user (مطلقة او ارملة او عزباء)
+
+    private String work_status_woman_man; // work status of the user (student, employee, etc.)
+
+    private String expected_marriage_date_woman; // expected marriage date of the user (في اسرع وقت او خلاص سنتين او لسه
+                                                 // مش متاكده)
+
+    private String fav_communication_woman_man; // favorite communication of the user (محادثه او محادثه وصوت او محادثه
+                                                // وصوت وصوره)
+
+    private String wearing_hijab_woman_man; // if the user wears hijab or not(محجبة او منتقبة او حره)
+
+    private String need_kids_woman_man; // if the user needs kids or not (بأسرع وقت ممكن او مع مرور الوقت )
+
+    private String smoking_drinking_woman_man; // if the user smokes or not (مدخنة او لا ادخن)
+
+    private String skin_woman_man; // skin of the user (white, black, etc.)
+
+    private String religious_commitment_woman_man; // religious commitment of the user (ملتزمة او مرنه او لا اهتم)
+
+    private ArrayList<String> daily_habits_woman; // daily habits of the user (احب القراءة او السباحه )
+    
+    private String weight_woman; // weight of the user
+
+    private String height_woman; // height of the user
+    
     private ArrayList<User> fav_user; // favorite users of the user
 }
+
