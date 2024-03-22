e9e20432f7611f842f35d360da5de43ece75c6b1
mostafa mahmoud <74399214+mostafa2742002@users.noreply.github.com>
2024-03-22 03:49:27 +0200

    Update UserService.java

--- src/main/java/com/marriage/app/service/UserService.java
+++ src/main/java/com/marriage/app/service/UserService.java
@@ -456,7 +456,7 @@ public class UserService {
             return ResponseEntity.notFound().build();
         }
 
-        if (marriageRepo.findByPhone(phone) != null) {
+        if (marriageRepo.findByPhone(phone).isPresent()) {
             return ResponseEntity.ok(true);
         } else {
             return ResponseEntity.ok(false);
