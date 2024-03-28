358e0ca2284e8c8ed614eacddd6e981e876e5492
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-03-29 01:23:16 +0200

    Add Twilio configuration and initialization***
    ***Update Twilio properties in application.properties

--- src/main/java/com/marriage/app/AppApplication.java
+++ src/main/java/com/marriage/app/AppApplication.java
@@ -2,12 +2,28 @@ package com.marriage.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marriage.app.config.TwilioConfig;
+import com.twilio.Twilio;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class AppApplication {
+
+	@Autowired
+	private TwilioConfig twilioConfig;
+
+	@PostConstruct
+	public void setup() {
+		Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-	//sudo systemctl start mongod
+	// sudo systemctl start mongod
 }
